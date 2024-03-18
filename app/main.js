a984c391ba6eb3d51411d3a5940153ba31b2935d
Haider Alshamma <haidera@nulogy.com>
2024-03-17 22:25:55 -0400

    Return PONG

--- app/main.js
+++ app/main.js
@@ -1,11 +1,9 @@
-const net = require("net");
+import net from "node:net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on("data", () => {
+    connection.write("+PONG\r\n");
+  });
 });
-//
+
 server.listen(6379, "127.0.0.1");
